fb1c588c655eaad8a440b08b41321ffdd417934e
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2021-12-27 00:46:20 +0900

    12/27
    plus nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,15 @@ const Header = () => {
     return (
         <header className="header">
         <h1>COVID-19</h1>
-            <select>
+        <ul className="nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Introduction</a></li>
+            <li><a href="#">Projects</a></li>
+        </ul>
+        <select>
             <option>국내</option>
             <option>세계</option>   
-            </select>
+        </select>
         </header>
     )
 }
